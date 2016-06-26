f95ac5151d3ad580d965937781a1f89a7ccb67eb
Olga Shapel <Snowhite@bk.ru>
2016-06-26 23:32:34 +0300

    добавила все действия

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,13 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
 var del = require("del");
+var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var run = require('run-sequence');
 
 gulp.task("copy", function() {
   return gulp.src([
@@ -35,18 +42,50 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: false
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "symbols",
+    fn
+  );
 });
 
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    ui: false
+    server: "build",
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
